5294e36250995ff2cb394b257749fc02f85f85eb
rafaellabimonte <rafaellabimonte09@gmail.com>
2025-08-21 09:19:33 -0300

    21.08

---
+++ aula06/src/Paginas/Exemplo1.jsx
@@ -0,0 +1,8 @@
+export default function Exemplo1()
+{
+    return (
+        <div>
+            <h1>Exemplo 1</h1>
+        </div>
+    )
+}
---
+++ aula06/src/Paginas/Exemplo2.jsx
@@ -0,0 +1,8 @@
+export default function Exemplo2()
+{
+    return (
+        <div>
+            <h1>Exemplo 2</h1>
+        </div>
+    )
+}

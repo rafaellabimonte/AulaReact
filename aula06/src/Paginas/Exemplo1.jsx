08188df52fdf646a36eebf47a1bdc80f46ecd3f5
rafaellabimonte <rafaellabimonte09@gmail.com>
2025-08-28 09:09:51 -0300

    29.08

--- aula06/src/Paginas/Exemplo1.jsx
+++ aula06/src/Paginas/Exemplo1.jsx
@@ -1,8 +1,58 @@
+import {Link} from 'react-router-dom';
+import { useState } from "react";
+
 export default function Exemplo1()
 {
-    return (
+    const [nome, setNome] = useState("Rafaella");
+    const [idade, setIdade] = useState(18);
+    const [resultado, setResultado] = useState(null);
+
+    function calcular()
+    {
+        let dias = Number(idade) * 365;
+
+        setResultado("Olá " + nome + ", você já viveu " + dias + " dias");
+    }
+
+    function limpar()
+    {
+        setNome("");
+        setIdade("");
+        setResultado("");
+    }
+
+        return (
         <div>
-            <h1>Exemplo 1</h1>
+            
+          <h1>Exemplo 1</h1>
+          <div className="container">
+          <form>
+              <p>
+                  Digite o nome do aluno <br />
+                  <input type="text" value={nome} onChange={(e) => setNome(e.target.value) }/>
+              </p>
+
+              <p>
+                  Digite a idade do aluno <br />
+                  <input type="text" value={idade} onChange={(e) => setIdade(e.target.value) }/>
+              </p>
+
+              <p>
+                  <input type="button" value="Exemplo 1" onClick={calcular}/>
+                  <input type="button" value="Limpar" onClick={limpar}/>
+              </p>
+
+              <p>
+                  <b>Resultados:</b><br />
+                  Nome: {nome} <br />
+                  Idade: {idade}
+              </p>
+
+              <p>
+                  {resultado}
+              </p>
+          </form>
+          </div>
         </div>
     )
 }
--- aula06/src/Paginas/Exemplo2.jsx
+++ aula06/src/Paginas/Exemplo2.jsx
@@ -1,8 +1,49 @@
+import {Link} from 'react-router-dom';
+import { useState } from "react";
+
 export default function Exemplo2()
 {
+    const [num1, setNum1] = useState(null);
+    const [num2, setNum2] = useState(null);
+    const [resultado, setResultado] = useState(null);
+
+    function calcular()
+    {
+        let dias = Number(idade) * 365;
+
+        setResultado();
+    }
+
     return (
         <div>
             <h1>Exemplo 2</h1>
+          <div className="container">
+          <form>
+              <p>
+                  Digite o número 1 <br />
+                  <input type="text" value={num1} onChange={(e) => setNum1(e.target.value) }/>
+              </p>
+
+              <p>
+                  Digite o número 2<br />
+                  <input type="text" value={num2} onChange={(e) => setNum2(e.target.value) }/>
+              </p>
+
+              <p>
+                  <input type="button" value="Calcular" onClick={calcular}/>
+
+                  <p>
+                  <b>Resultado:</b><br />
+                  A soma é: {soma} <br />
+                  A subtração é: {sub} 
+                  A multiplicação é: {mult}
+                  A divisão é: {div}
+                  A exponenciação é {expo}
+                  O resto da divisão é: {resto}
+                </p>
+            </p>
+                </form>
+        </div>
         </div>
     )
 }
